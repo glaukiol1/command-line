543a6b5a55474b8b1a066c08df5fda1ab5661f8c
glaukiol1 <glaukiol1@gmail.com>
2021-06-12 00:11:46 +0200

    Updated the '.help' command

--- cmds/help.hh
+++ cmds/help.hh
@@ -4,7 +4,7 @@
 namespace help {
     class cmd_help {
         public:
-            std::string help = "cmdline HELP: \n \t echo <message> | print a message to the console. \n\t ------------------------- \n\t @glaukiol1";
+            std::string help = "cmdline HELP: \n \t echo <message> | print a message to the console. \n \t ls | get the files and subdirectories of the current working directory. \n \t clear | Clear the shell. \n\t ------------------------- \n\t @glaukiol1";
             void run() {
                 std::cout << help;
             };
